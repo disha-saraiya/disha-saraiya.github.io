60d44405dea3b208a5bbca1b556bb5dae286fa3b
dishasaraiya <saraiya.di@husky.neu.edu>
2020-08-05 17:25:23 -0500

    Updates

---
+++ precache-manifest.2a7f96d51a55690a8f6e74faeca73589.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74d3f90b6af561b0f2963ce35e2f962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6797356d60eb6c1231bf",
+    "url": "/static/css/2.51964e6d.chunk.css"
+  },
+  {
+    "revision": "8fd16d87419ac9b67993",
+    "url": "/static/css/main.b1d84f27.chunk.css"
+  },
+  {
+    "revision": "6797356d60eb6c1231bf",
+    "url": "/static/js/2.bd32258c.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.bd32258c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd16d87419ac9b67993",
+    "url": "/static/js/main.499cc847.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b95c29b14ecd42d065397724ff415720",
+    "url": "/static/media/comingsoon.b95c29b1.gif"
+  },
+  {
+    "revision": "08941f5ccd85f6a17256058b87b2889e",
+    "url": "/static/media/covidtracker.08941f5c.PNG"
+  },
+  {
+    "revision": "4a5aa7b59769022bb0d9283bb0259d21",
+    "url": "/static/media/dev-disha-02.4a5aa7b5.jpg"
+  },
+  {
+    "revision": "16aa216dddd2f1e66d54838b33b73e2f",
+    "url": "/static/media/favicon.16aa216d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "edcff348277f377314e62b05620c1e6b",
+    "url": "/static/media/h1bproject.edcff348.PNG"
+  },
+  {
+    "revision": "c2358039bd651f97ce2dacd7d732f359",
+    "url": "/static/media/recipeapp.c2358039.PNG"
+  },
+  {
+    "revision": "55fdf5ce61cdc2c6d54ae5a2974be68a",
+    "url": "/static/media/resume.55fdf5ce.pdf"
+  },
+  {
+    "revision": "3e6ddf9defd21941cc929b1328bafb6e",
+    "url": "/static/media/unnatiapp.3e6ddf9d.PNG"
+  }
+]);
